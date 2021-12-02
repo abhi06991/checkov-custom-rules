cb4c40a571be22175e10d15a48187de0d480a921
abhi06991 <abhi06991@users.noreply.github.com>
2021-12-02 05:06:32 -0500

    Add files via upload

---
+++ vulnerable-tf-files/aws-internet-gateway.tf
@@ -0,0 +1,63 @@
+resource "aws_internet_gateway" "*#$%^#$%#" {
+  vpc_id = aws_vpc.web_vpc.id
+  *sdfwfew!@#$!$!@#$!
+  tags = merge({
+    Name = "${local.resource_prefix.value}-igw"
+    }, {
+    git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
+    git_file             = "terraform/aws/ec2.tf"
+    git_last_modified_at = "2020-06-16 14:46:24"
+    git_last_modified_by = "nimrodkor@gmail.com"
+    git_modifiers        = "nimrodkor"
+    git_org              = "bridgecrewio"
+    git_repo             = "terragoat"
+    yor_trace            = "d8e63cb4-2fb5-4726-9c86-5fd05ef03674"
+  })
+}
+
+data "aws_iam_policy_document" "example" {
+  statement {
+    sid = "1"
+
+    actions = [
+      "s3:ListAllMyBuckets",
+      "s3:GetBucketLocation",
+    ]
+
+    resources = [
+      "arn:aws:s3:::*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.s3_bucket_name}",
+    ]
+
+    condition {
+      test     = "StringLike"
+      variable = "s3:prefix"
+
+      values = [
+        "",
+        "home/",
+        "home/&{aws:username}/",
+      ]
+    }
+  }
+
+  statement {
+    actions = [
+      "s3:*",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.s3_bucket_name}/home/&{aws:username}",
+      "arn:aws:s3:::${var.s3_bucket_name}/home/&{aws:username}/*",
+    ]
+  }
+}
