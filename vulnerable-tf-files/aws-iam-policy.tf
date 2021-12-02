341087a9d201d2c45c90852c27da6440b959f8ea
abhi06991 <abhi06991@users.noreply.github.com>
2021-12-02 05:05:56 -0500

    Update aws-iam-policy.tf

--- vulnerable-tf-files/aws-iam-policy.tf
+++ vulnerable-tf-files/aws-iam-policy.tf
@@ -1,1 +1,23 @@
-asfdasfadsf
+resource "aws_iam_policy" "policy" {
+  name        = "IAM Resource Policy"
+  path        = "/"
+  description = "Applies to IAM Resources"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+          "s3:df:d:asdad:asd:asd:*",
+          "lambda:as*",
+          "logs:dsd",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
