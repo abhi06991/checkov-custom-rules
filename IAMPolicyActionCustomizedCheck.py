b7a6a466fb33b37a6a99dfbec48e207d5b130c67
abhi06991 <abhi06991@users.noreply.github.com>
2021-12-02 04:51:50 -0500

    Add files via upload

---
+++ IAMPolicyActionCustomizedCheck.py
@@ -0,0 +1,42 @@
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+from checkov.common.models.enums import CheckResult, CheckCategories
+
+
+class IAMPolicyActionCustomizedCheck(BaseResourceCheck):
+    def __init__(self):
+        name = "Ensure no IAM policies allow “*” as a statement’s actions"
+        id = "CKV_AWS_63_Customized"
+        supported_resources = ['aws_iam_role_policy', 'aws_iam_user_policy', 'aws_iam_group_policy', 'aws_iam_policy','aws_ssoadmin_permission_set_inline_policy']
+        # CheckCategories are defined in models/enums.py
+        categories = [CheckCategories.IAM]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+    def scan_resource_conf(self, conf):
+        if 'policy' in conf.keys():
+            if 'Statement' in conf['policy'][0]:
+                policyStatement = conf['policy'][0]['Statement'][0]
+                for index,values in enumerate(policyStatement):
+                    if values == 'Action':
+                        actionValue = policyStatement[values]
+                        if type(actionValue) == str:
+                            if actionValue == "*":
+                                return CheckResult.FAILED
+                            else:
+                                return CheckResult.PASSED
+                        elif type(actionValue) == list:
+                            resourceHasWildcardValue = 'false'
+                            for index1,values1 in enumerate(actionValue):
+                                splitString = values1.split(':')
+                                for index2,value2 in enumerate(splitString):
+                                    if value2 == '*':
+                                        resourceHasWildcardValue = 'true'
+                            if resourceHasWildcardValue == 'true':
+                                return CheckResult.FAILED
+                            else:
+                                return CheckResult.PASSED
+
+            else:
+                return None
+        else:
+            return None
+
+scanner = IAMPolicyActionCustomizedCheck()
